5db00eccf265edf93a9a37dea2b4c694bf58ca76
Beckyshee <wanjirubecky.rw@gmail.com>
2023-11-27 21:09:25 +0300

    token

---
+++ backend/src/config/sqlConfig.ts
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+import mssql from 'mssql';
+
+dotenv.config();
+
+export const sqlConfig = {
+  user: process.env.DB_USER as string,
+  password: process.env.DB_PWD as string,
+  database: process.env.DB_NAME as string,
+  server: 'localhost',
+  pool: {
+    max: 10,
+    min: 0, 
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: false, 
+    trustServerCertificate: true 
+  }
+}
+
+export async function testConnection() {
+    console.log(process.env.DB_NAME);
+  
+    const pool = await mssql.connect(sqlConfig);
+    //testing if db is connected
+    if (pool.connected) {
+      console.log("connected to database");
+    } else {
+      console.log("connection failed");
+    }
+  }
+  
+
+  testConnection()
